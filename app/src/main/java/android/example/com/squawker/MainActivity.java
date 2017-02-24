af09773d5d6ade1f29c827e76a75354636783eff
Lyla <lyla@udacity.com>
2017-02-24 03:18:12 +0200

    TFCM.02-Solution-TestGettingExtraData
    (cherry picked from commit e3430d0)

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -86,8 +86,12 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
+        // Gets the extra data from the intent that started the activity. For *notification*
+        // messages, this will contain key value pairs stored in the *data* section of the message.
         Bundle extras = getIntent().getExtras();
-        if(extras != null && extras.containsKey("test")){
+        // Checks if the extras exist and if the key "test" from our FCM message is in the intent
+        if (extras != null && extras.containsKey("test")) {
+            // If the key is there, print out the value of "test"
             Log.d(LOG_TAG, "Contains: " + extras.getString("test"));
         }
 
