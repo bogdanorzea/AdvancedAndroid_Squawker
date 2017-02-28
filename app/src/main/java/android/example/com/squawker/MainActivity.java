ba54f12829a480ba7ee19d2f4007e659b7714a26
Lyla <lyla@udacity.com>
2017-02-28 03:58:47 +0200

    TFCM.04-Solution-AddFirebaseMessagingService
    (cherry picked from commit 548043b)

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -87,6 +87,12 @@ public class MainActivity extends AppCompatActivity implements
 
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
+
+        // Get token from the ID Service you created and show it in a log
+        String token = FirebaseInstanceId.getInstance().getToken();
+        String msg = getString(R.string.message_token_format, token);
+        Log.d(LOG_TAG, msg);
+
     }
 
     @Override
